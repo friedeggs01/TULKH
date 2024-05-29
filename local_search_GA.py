5b96f96a1bb9e32b1bf14be9662d0353cb9ca8d9
HungTT912 <noohung2003@gmail.com>
2024-05-30 00:17:00 +0700

    rlsGA

--- local_search_GA.py
+++ local_search_GA.py
@@ -1,4 +1,5 @@
 import random
+from time import time 
 
 def calculate_total_time(assignments, repair_times, travel_times):
     max_time = 0
@@ -64,6 +65,35 @@ def mutation(assignments, mutation_rate):
 
     return mutated_assignments
 
+def ox_crossover(parent1, parent2):
+    child_routes = {k: v[:] for k, v in parent2.items()}
+    
+    # Randomly choose a subroute from a random route in parent1
+    #print(parent1)
+    parent1_route_keys = list(parent1.keys())
+    parent1_route_idx = random.randint(0, len(parent1_route_keys) - 1)
+    parent1_route = parent1[parent1_route_keys[parent1_route_idx]]
+    if not parent1_route:
+        return child_routes
+    start = random.randint(0, len(parent1_route) - 1)
+    end = random.randint(start, len(parent1_route) - 1)
+    subroute = parent1_route[start:end+1]
+    
+    # Find a random position to insert the subroute in the child
+    child_route_keys = list(child_routes.keys())
+    child_route_idx = random.randint(0, len(child_route_keys) - 1)
+    insert_pos = random.randint(0, len(child_routes[child_route_keys[child_route_idx]]))
+    
+    # Remove the customers in the subroute from the child routes
+    for customer in subroute:
+        for key, route in child_routes.items():
+            if customer in route:
+                child_routes[key].remove(customer)
+    
+    # Insert the subroute into the child
+    child_routes[child_route_keys[child_route_idx]][insert_pos:insert_pos] = subroute
+    
+    return child_routes
 
 def local_search_with_ga(customers, repair_times, travel_times, num_employees, population_size, num_generations, mutation_rate):
     population = generate_initial_population(customers, num_employees, population_size)
@@ -77,7 +107,7 @@ def local_search_with_ga(customers, repair_times, travel_times, num_employees, p
 
         for assignments in population:
             # Perform local search on each individual
-            for _ in range(1000):  # Local search iterations
+            for _ in range(10):  # Local search iterations
                 neighbors = generate_neighbors(assignments,num_employees)
                 best_emp = None
                 best_time_diff = float('inf')
@@ -103,39 +133,103 @@ def local_search_with_ga(customers, repair_times, travel_times, num_employees, p
         while len(new_population) < population_size:
             parent1 = selection(population, fitness_scores)
             parent2 = selection(population, fitness_scores)
-            child1, child2 = parent1.copy(), parent2.copy()
-            mutation(child1, mutation_rate)
-            mutation(child2, mutation_rate)
+            child1 = ox_crossover(parent1, parent2)
+            child2 = ox_crossover(parent2, parent1) 
+            child1 = mutation(child1, mutation_rate)
+            child2 = mutation(child2, mutation_rate)
             new_population.extend([child1, child2])
 
         population = new_population
     
     return best_assignments, calculate_total_time(best_assignments, repair_times, travel_times)
 
-# Example usage
-fi = open('./data/data_10_5.txt','r') 
-num_customers, num_employees = fi.readline().split(' ') 
-num_customers, num_employees = int(num_customers), int(num_employees) 
-customers = [i+1 for i in range(num_customers)]
-d = fi.readline().split(' ') 
-repair_times = { i+1: int(d[i]) for i in range(num_customers)}
-travel_times = [[] for i in range(num_customers+1)]
-for i in range(num_customers+1): 
-    tmp = fi.readline().split(' ')
-    travel_times[i] = [int(tmp[i]) for i in range(num_customers+1)]
-population_size = 50  # Size of the population
-num_generations = 100  # Number of generations
-mutation_rate = 0.2  # Probability of mutation
 
-best_assignments, best_time = local_search_with_ga(
-    customers,
-    repair_times,
-    travel_times,
-    num_employees,
-    population_size,
-    num_generations,
-    mutation_rate
-)
+# Test with file
+if __name__=="__main__": 
+    inp = ['data_50_5.txt','data_50_7.txt','data_50_10.txt','data_50_20.txt','data_50_25.txt']
+    for s in inp: 
+        path_inp = './data/'+s 
+        path_out = './result/result_local_search_GA/'+s
+        fi = open(path_inp,'r') 
+        output = open(path_out,'w')
+        num_customers, num_employees = fi.readline().split(' ') 
+        num_customers, num_employees = int(num_customers), int(num_employees) 
+        customers = [i+1 for i in range(num_customers)]
+        d = fi.readline().split(' ') 
+        repair_times = { i+1: int(d[i]) for i in range(num_customers)}
+        
+        travel_times = [[] for i in range(num_customers+1)]
+        for i in range(num_customers+1): 
+            tmp = fi.readline().split(' ')
+            travel_times[i] = [int(tmp[i]) for i in range(num_customers+1)]
+        
+        starttime = time() 
+        population_size = 50  # Size of the population
+        num_generations = 100  # Number of generations
+        mutation_rate = 0.2  # Probability of mutation
+        
+        assignments, best_time = local_search_with_ga(
+            customers,
+            repair_times,
+            travel_times,
+            num_employees,
+            population_size,
+            num_generations,
+            mutation_rate
+        )
+        endtime = time() 
+        output.write("Objective value: "+str(best_time)+'\n')
+        print("Running time "+str(endtime-starttime))
+        output.write("Running time "+str(endtime-starttime)+'\n') 
+        print("Minimum Total Time:", best_time)
+        output.write("Solution"+'\n')
+        print(num_employees)
+        for emp in assignments.keys(): 
+            print(len(assignments[emp]))
+            print(0,end=' ') 
+            output.write(str(0)+' ') 
+            for task in assignments[emp]: 
+                print(task,end=' ') 
+                output.write(str(task)+' ')
+            print(0)
+            output.write(str(0)+'\n')
+        output.close()
+        print("Best Assignments:", assignments)
+#Test with IO standard
+'''
 
-print("Best Assignments:", best_assignments)
-print("Minimum Total Time:", best_time)
+if __name__=="__main__": 
+
+    num_customers, num_employees = input().split(' ')
+    num_customers, num_employees = int(num_customers), int(num_employees) 
+    customers = [i+1 for i in range(num_customers)]
+    #d = fi.readline().split(' ') 
+    d = input().split(' ') 
+    repair_times = { i+1: int(d[i]) for i in range(num_customers)}
+    
+    travel_times = [[] for i in range(num_customers+1)]
+    for i in range(num_customers+1): 
+        #tmp = fi.readline().split(' ')
+        tmp = input().split(' ')
+        travel_times[i] = [int(tmp[i]) for i in range(num_customers+1)]
+    population_size = 50  # Size of the population
+    num_generations = 100  # Number of generations
+    mutation_rate = 0.2  # Probability of mutation
+    
+    assignments, best_time = local_search_with_ga(
+        customers,
+        repair_times,
+        travel_times,
+        num_employees,
+        population_size,
+        num_generations,
+        mutation_rate
+    )
+    print(num_employees)
+    for emp in assignments.keys(): 
+        print(len(assignments[emp])+2)
+        print(0,end=' ') 
+        for task in assignments[emp]: 
+            print(task,end=' ') 
+        print(0)
+'''
